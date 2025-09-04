cfee6d029df94f5b2073b5c30579df98a7c1a383
Leandro Gonzalez <leanlag@gmail.com>
2025-09-04 18:11:16 -0300

    Implementar servicio Estudiante con métodos para listar, buscar, guardar y eliminar estudiantes.

--- Java/Clase04/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Clase04/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,5 +1,41 @@
 package utn.estudiantes.servicio;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
+
+@Service // Indica que esta clase es un servicio de Spring
 public class EstudianteServicio implements IEstudianteServicio {
+    @Autowired // Inyecta la dependencia del repositorio de estudiantes
+    private EstudianteRepositorio estudianteRepositorio; // Repositorio de estudiantes
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        // Lógica para listar todos los estudiantes
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        // Lógica para buscar un estudiante por su ID
+        return estudianteRepositorio.findById(idEstudiante).orElse(null);
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        // Lógica para guardar un estudiante
+        estudianteRepositorio.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        // Lógica para eliminar un estudiante
+        estudianteRepositorio.delete(estudiante);
+    }
 
 }
