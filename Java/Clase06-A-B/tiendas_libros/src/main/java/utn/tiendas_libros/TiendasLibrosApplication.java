5d3d5bff3b3fc62804d999f8c26ca60c970151ef
Denis Taraboreli <dtaraboreli@gmail.com>
2025-10-08 12:58:51 -0300

    Java: clase 6 A - B y clase 7

---
+++ Java/Clase06-A-B/tiendas_libros/src/main/java/utn/tiendas_libros/TiendasLibrosApplication.java
@@ -0,0 +1,13 @@
+package utn.tiendas_libros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendasLibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendasLibrosApplication.class, args);
+	}
+
+}
