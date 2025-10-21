7f44adbbe72ec9d9bf7a716d32e1f40523693d8e
Leandro Gonzalez <leanlag@gmail.com>
2025-10-21 10:48:18 -0300

    Clase 06 de Node Cuarto Semestre

---
+++ Node/Clase06/entorno.js
@@ -0,0 +1,8 @@
+let nombre = process.env.NOMBRE || "Sin Nombre";
+let web = process.env.WEB || "No tengo web";
+
+console.log("Nombre:", nombre);
+console.log("Web:", web);
+
+console.log("Hola desde Node.js");
+//console.log(`Hola ${nombre}`);
